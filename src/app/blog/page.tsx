f07deaa5cc4466c4936c2aa148c4c99e91de23eb
rahfianugerah <nrahfia@gmail.com>
2025-07-24 17:46:47 +0700

    faet: update the blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -13,34 +13,47 @@ export default async function BlogPage() {
   const posts = await getBlogPosts();
 
   return (
-    <section>
-      <BlurFade delay={BLUR_FADE_DELAY}>
-        <h1 className="font-medium text-2xl mb-8 tracking-tighter">blog</h1>
-      </BlurFade>
-      {posts
-        .sort((a, b) => {
-          if (
-            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
-          ) {
-            return -1;
-          }
-          return 1;
-        })
-        .map((post, id) => (
-          <BlurFade delay={BLUR_FADE_DELAY * 2 + id * 0.05} key={post.slug}>
-            <Link
-              className="flex flex-col space-y-1 mb-4"
-              href={`/blog/${post.slug}`}
-            >
-              <div className="w-full flex flex-col">
-                <p className="tracking-tight">{post.metadata.title}</p>
-                <p className="h-6 text-xs text-muted-foreground">
-                  {post.metadata.publishedAt}
-                </p>
-              </div>
-            </Link>
-          </BlurFade>
-        ))}
+    <section id="blog">
+      <div className="space-y-12 w-full py-12">
+        <BlurFade delay={BLUR_FADE_DELAY * 11}>
+          <div className="flex flex-col items-center justify-center space-y-4 text-center">
+            <div className="space-y-2">
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                Rahfi's Writings
+              </h2>
+              <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                I enjoy sharing my thoughts and experiences through writing. Here are some of my
+                recent blog posts where I discuss various topics related to software development,
+                technology, and life in general.
+              </p>
+            </div>
+          </div>
+        </BlurFade>
+        {posts
+          .sort((a, b) => {
+            if (
+              new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
+            ) {
+              return -1;
+            }
+            return 1;
+          })
+          .map((post, id) => (
+            <BlurFade delay={BLUR_FADE_DELAY * 2 + id * 0.05} key={post.slug}>
+              <Link
+                className="flex flex-col space-y-1 mb-4"
+                href={`/blog/${post.slug}`}
+              >
+                <div className="w-full flex flex-col">
+                  <p className="tracking-tight">{post.metadata.title}</p>
+                  <p className="h-6 text-xs text-muted-foreground">
+                    {post.metadata.publishedAt}
+                  </p>
+                </div>
+              </Link>
+            </BlurFade>
+          ))}
+      </div>
     </section>
   );
 }
