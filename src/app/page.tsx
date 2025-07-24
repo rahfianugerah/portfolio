e0c6e1fcd7eaa7761934b780476b1cb2da8cd7ea
rahfianugerah <nrahfia@gmail.com>
2025-07-24 17:29:18 +0700

    feat: update the hardwork section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,15 +145,12 @@ export default function Page() {
                   Hardwork & Achievements
                 </div>
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  I like building things
+                  Indefatigability
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  During my time in university, I attended{" "}
-                  {DATA.hardwork.length}+ hardwork. People from around the
-                  country would come together and build incredible things in 2-3
-                  days. It was eye-opening to see the endless possibilities
-                  brought to life by a group of motivated and passionate
-                  individuals.
+                  I have participated in various events, where I have
+                  honed my skills and achieved significant milestones. Here are some of my
+                  notable results.
                 </p>
               </div>
             </div>
