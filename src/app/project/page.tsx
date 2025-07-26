5b8eb573bec9c5bc133791b75234d53d67a5ba96
rahfianugerah <nrahfia@gmail.com>
2025-07-26 13:31:26 +0700

    feat: create section for my certifications

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -5,49 +5,69 @@ import { DATA } from "@/data/resume";
 const BLUR_FADE_DELAY = 0.04;
 
 export const metadata = {
-  title: "Projects",
-  description: "A showcase of my work and collaborations.",
+    title: "Projects",
+    description: "A showcase of my work and collaborations.",
 };
 
 export default function ProjectPage() {
     return (
-        <section id="projects">
-            <div className="space-y-12 w-full py-12">
-                <BlurFade delay={BLUR_FADE_DELAY * 11}>
-                    <div className="flex flex-col items-center justify-center space-y-4 text-center">
-                        <div className="space-y-2">
-                            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                                Rahfi's Projects
-                            </h2>
-                            <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                                I have worked on various projects, both personal and collaborative,
-                                that showcase my skills and creativity. Here are some of my notable
-                                projects that I am proud to share.
-                            </p>
+        <>
+            <section id="projects">
+                <div className="space-y-12 w-full py-12">
+                    <BlurFade delay={BLUR_FADE_DELAY * 11}>
+                        <div className="flex flex-col items-center justify-center space-y-4 text-center">
+                            <div className="space-y-2">
+                                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                                    Rahfi's Projects
+                                </h2>
+                                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                                    I have worked on various projects, both personal and collaborative,
+                                    that showcase my skills and creativity. Here are some of my notable
+                                    projects that I am proud to share.
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                </BlurFade>
-                <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
-                    {DATA.projects.map((project, id) => (
-                        <BlurFade
-                            key={project.title}
-                            delay={BLUR_FADE_DELAY * 12 + id * 0.05}
-                        >
-                            <ProjectCard
-                                href={project.href}
+                    </BlurFade>
+                    <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+                        {DATA.projects.map((project, id) => (
+                            <BlurFade
                                 key={project.title}
-                                title={project.title}
-                                description={project.description}
-                                dates={project.dates}
-                                tags={project.technologies}
-                                image={project.image}
-                                video={project.video}
-                                links={project.links}
-                            />
-                        </BlurFade>
-                    ))}
+                                delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+                            >
+                                <ProjectCard
+                                    href={project.href}
+                                    key={project.title}
+                                    title={project.title}
+                                    description={project.description}
+                                    dates={project.dates}
+                                    tags={project.technologies}
+                                    image={project.image}
+                                    video={project.video}
+                                    links={project.links}
+                                />
+                            </BlurFade>
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            <section id="certifications">
+                <div className="space-y-12 w-full py-12">
+                    <BlurFade delay={BLUR_FADE_DELAY * 11}>
+                        <div className="flex flex-col items-center justify-center space-y-4 text-center">
+                            <div className="space-y-2">
+                                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                                    Rahfi's Certifications
+                                </h2>
+                                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                                    I have obtained various certifications that demonstrate my expertise
+                                    and commitment to continuous learning. Here are some of the certifications
+                                    that I am proud to showcase.
+                                </p>
+                            </div>
+                        </div>
+                    </BlurFade>
+                </div>
+            </section>
+        </>
     );
 };
